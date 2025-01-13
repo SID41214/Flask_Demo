142f7f1cd853c631cc1884aa081707adc2c80aba
Aboobacker Sidheeque <sidhub41214@gmail.com>
2025-01-13 07:31:53 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #relative path
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255),nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+    
 
 @app.route('/')
 
@@ -8,5 +22,8 @@ def index():
     # return "Hello, World!"
     return render_template('index.html')
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
