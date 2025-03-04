853fd19df39bb5b390b463bd36020337508602ee
Aboobacker Sidheeque <sidhub41214@gmail.com>
2025-03-04 13:58:01 +0530

    delete button

--- app.py
+++ app.py
@@ -35,6 +35,17 @@ def index():
     
     
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an issue deleting that task'
+    
+
 
 
 if __name__ == '__main__':
